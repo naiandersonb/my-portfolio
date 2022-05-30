a6d22728b380beac0756af746699bfe65bd0e875
naiandersonb <nayanderson2014@gmail.com>
2022-05-30 09:21:32 -0300

    corrigindo links

--- src/components/SocialLinks/index.tsx
+++ src/components/SocialLinks/index.tsx
@@ -7,21 +7,25 @@ type Props = {
 export const SocialLinks = ({ direction }: Props) => {
   return (
     <Group direction={direction} >
+
       <Anchor href='https://www.instagram.com/naiandersonbruno_/' rel="noreferrer" target='_blank' >
         <ActionIcon color="dark" size="lg" variant="filled" >
-          <BsGithub />
-        </ActionIcon>
-      </Anchor>
-      <Anchor href='https://github.com/naiandersonb' rel="noreferrer" target='_blank'>
-        <ActionIcon color="dark" size="lg" variant="filled">
           <BsInstagram />
         </ActionIcon>
       </Anchor>
+
+      <Anchor href='https://github.com/naiandersonb' rel="noreferrer" target='_blank'>
+        <ActionIcon color="dark" size="lg" variant="filled">
+          <BsGithub />
+        </ActionIcon>
+      </Anchor>
+
       <Anchor href='https://www.linkedin.com/in/naianderson-bruno-franca/' rel="noreferrer" target='_blank'>
         <ActionIcon color="dark" size="lg" variant="filled">
           <BsLinkedin />
         </ActionIcon>
       </Anchor>
+
     </Group>
   )
 }
